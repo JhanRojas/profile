d39d4f4e1ba176dd5b11fe7fe683d1b5eb9fe4e9
Jhan Rojas <jhanrojasbuitron@gmail.com>
2018-01-04 21:49:53 -0500

    adding template

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-$("#btnSaludar").click(function(){
-	alert("Hola");
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+		scrollBar: false,
+		navigation: true,
+		verticalCentered: false
+	});
 });
