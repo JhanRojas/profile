1ee60c4a7a5c3d3903c46385261ce41c2640881e
Jhan Rojas <jhanrojasbuitron@gmail.com>
2018-02-10 22:35:35 -0500

    adding grunt critical

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,16 +23,33 @@ module.exports = function(grunt) {
       html: {
         src: './index.html', dest: 'dist/index.html'
       }
+    },
+    critical: {
+      test: {
+          options: {
+              base: './dist',
+              css: [
+                  'dist/styles/main.css',
+                  'dist/styles/vendor.css'
+              ],
+              width: 320,
+              height: 70
+          },
+          src: 'dist/index.html',
+          dest: 'dist/index.html'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-critical');
   grunt.registerTask('build',[
     'copy:html',
     'useminPrepare',
     'concat',
     'uglify',
     'cssmin',
-    'usemin'
+    'usemin',
+    'critical'
     ]);
 };
